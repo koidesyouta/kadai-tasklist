c8ad54476a2af526ae2f8b3aa23502120bdba6c3
shouta.koide <koide@valuedesign.jp>
2024-04-23 16:08:28 +0900

    訂正

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -28,7 +28,7 @@ public class EditServlet extends HttpServlet {
 
         em.close();
 
-        request.setAttribute("message", t);
+        request.setAttribute("task", t);
         request.setAttribute("_token", request.getSession().getId());
 
         request.getSession().setAttribute("task_id", t.getId());
